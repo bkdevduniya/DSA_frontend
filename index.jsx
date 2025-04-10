8731d36f633826aa09ccb9cb1ae29d78731595fd
bkdevduniya <bhoodev6232@gmail.com>
2025-04-11 01:33:29 +0530

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -9,7 +9,7 @@ import Problemset from "./src/problemset.jsx";
 import Profile from "./src/profile.jsx";
 import Auth from "./src/auth.jsx";
 import Sheets from "./src/sheets.jsx";
-
+import Recommend from "./src/recommendation.jsx";
 // Standalone pages
 import Login from "./src/login.jsx";
 import Signup from "./src/signup.jsx";
@@ -27,11 +27,10 @@ const router = createBrowserRouter(
         <Route index element={<Home />} />
         <Route path="problemset" element={<Problemset />} />
         <Route path="profile" element={<Profile />} />
-        <Route path="auth" element={<Auth />} />
+        <Route path='recommended' element={<Recommend />} />
         <Route path="sheets" element={<Sheets />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Route>
-      
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/admin" element={<Admin />} />
