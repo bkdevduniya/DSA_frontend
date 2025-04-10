c34755585bf6e29d548d907bb13d851b64ad1fa5
bkdevduniya <bhoodev6232@gmail.com>
2025-04-10 12:00:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
-
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: true, // allows external access (e.g., via loca.lt)
-  //   port: 5173, // or whatever port you're using
-  //   strictPort: true, // fail if port is taken
-  //   cors: true, // enable CORS
-  //   origin: 'https://5g67brzk-5173.inc1.devtunnels.ms', // optional: sets origin
-  //   hmr: {
-  //     host: '5g67brzk-5173.inc1.devtunnels.ms',
-  //   },
-  // },
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: true,
+  },
+});
